cb515b45b3308b3520df1ce715c4f888bddca844
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2017-03-30 22:33:28 -0700

    Added another text field

--- index.ios.js
+++ index.ios.js
@@ -36,7 +36,18 @@ export default class Glance extends Component {
                           onChangeText={(text) => this.setState({text})}
                         />
                         <Text style={{padding: 10, fontSize: 42}}>
-                          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+                          {this.state.text}
+                        </Text>
+                    </View>
+
+                    <View style={{padding: 10}}>
+                        <TextInput
+                          style={{height: 40}}
+                          placeholder="Type here to translate!"
+                          onChangeText={(text) => this.setState({text})}
+                        />
+                        <Text style={{padding: 10, fontSize: 42}}>
+                          {this.state.text}
                         </Text>
                     </View>
                 </View>
