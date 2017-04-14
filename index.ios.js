5a7e183d78cd2425d2592005907fda6b8eb49902
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2017-04-14 14:23:48 -0700

    fixed the header

--- index.ios.js
+++ index.ios.js
@@ -22,54 +22,59 @@ export default class Glance extends Component {
         this.state = {text: ''};
     }
 
+    _onScroll(e) {
+
+    }
+
     render() {
         return (
-            <ScrollView style={{flex: 1}}>
-                <View style={{height: 50, backgroundColor: 'powderblue'}} />
+            <View style={styles.container}>
+                <View style={styles.header} />
 
-                <View style={{
-                  flex: 1,
-                  flexDirection: 'column'
-                }}>
-
-                    <View style={{padding: 20}}>
-                        <AutoGrowingTextInput
-                          style={{ height: 40, fontSize: 20}}
-                          placeholder="Type Question"
-                          onChangeText={(text) => this.setState({text})}
-                        />
-                    </View>
-
+                <ScrollView style={styles.container}
+                  onScroll={this._onScroll}
+                >
                     <View style={{
-                      margin: 20,
-                      height: 1,
-                      backgroundColor: 'powderblue'
-                    }}></View>
+                      flex: 1,
+                      flexDirection: 'column'
+                    }}>
 
-                    <View style={{padding: 20}}>
-                        <AutoGrowingTextInput
-                          style={{height: 40, fontSize: 20}}
-                          placeholder="Type Answer"
-                          onChangeText={(text) => this.setState({text})}
-                        />
+                        <View style={{padding: 20}}>
+                            <AutoGrowingTextInput
+                              style={{ height: 40, fontSize: 20}}
+                              placeholder="Type Question"
+                              onChangeText={(text) => this.setState({text})}
+                            />
+                        </View>
+
+                        <View style={{
+                          margin: 20,
+                          height: 1,
+                          backgroundColor: 'powderblue'
+                        }}></View>
+
+                        <View style={{padding: 20}}>
+                            <AutoGrowingTextInput
+                              style={{height: 40, fontSize: 20}}
+                              placeholder="Type Answer"
+                              onChangeText={(text) => this.setState({text})}
+                            />
+                        </View>
                     </View>
-                </View>
-            </ScrollView>
+
+                </ScrollView>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        flex: 1
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+    header: {
+      height: 50,
+      backgroundColor: 'powderblue'
     },
     instructions: {
         textAlign: 'center',
